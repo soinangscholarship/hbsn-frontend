ccff2f74ac536213ef66967ba9551323b862e313
soinangscholarship <contact.hbsn@nguyenhienfoundation.org>
2025-09-28 23:20:16 +0700

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Sợi Nắng 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
