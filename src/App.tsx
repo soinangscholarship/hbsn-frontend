c4dd27eaf5efd0efaf6eacf29368b42cf1f7b968
soinangscholarship <contact.hbsn@nguyenhienfoundation.org>
2025-09-29 22:47:11 +0700

    Prepare for GitHub Pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Routes, Route, Link } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Sợi Nắng 🚀
-      </h1>
+    <div>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hbsn-frontend/',  // 👈 rất quan trọng
-})
+  base: "/hbsn-frontend/",
+});
