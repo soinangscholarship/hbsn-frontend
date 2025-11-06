3fb19c8f04990821c4aff9172b5ce7dca52cfd39
1N6M1Q0 <ngminhquan161004@gmail.com>
2025-11-06 14:08:10 +0700

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./components/pages/HomePage/HomePage";
 import NavBar from "./components/layout/NavBar/NavBar";
 import Footer from "./components/layout/Footer/Footer";
+import ScholarshipPage from "./components/pages/ScholarshipPage/ScholarshipPage";
+import AboutPage from "./components/pages/AboutPage/AboutPage";
+import ActivitiesPage from "./components/pages/ActivitiesPage/ActivitiesPage";
 
 function App() {
   return (
@@ -9,8 +12,11 @@ function App() {
       <NavBar />
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/scholarship" element={<ScholarshipPage />} />
+          <Route path="/activities" element={<ActivitiesPage />} />
         </Routes>
       </main>
       <Footer />
---
+++ src/components/pages/ActivitiesPage/ActivitiesPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ActivitiesPage.css";
+
+const ActivitiesPage: React.FC = () => {
+
+  return (
+    <div className="homepage-container">Activities Page</div>
+  );
+};
+
+export default ActivitiesPage;
